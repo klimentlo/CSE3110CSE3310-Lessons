74d35487cacc1d8e34d59a18207719c95b679494
k.lo1 <k.lo1@share.epsb.ca>
2023-09-06 11:47:03 -0600

    created the linear search and stats

---
+++ a_linear_search.py
@@ -0,0 +1,42 @@
+#a_linear_search.py
+
+'''
+title: a_linear_search.py
+author: Kliment Lo
+date: 2023-09-06
+'''
+
+import random
+import time
+import statistics
+
+### -- Inputs
+
+### -- Processing
+# Make a large data set
+NUMBERS = []
+for i in range(20000000):
+    if random.randrange(2) == 1:
+        NUMBERS.append(i)
+print("Done generating")
+
+TRIALS = []
+
+for i in range(30):
+    # Choose a random number
+    NUMBER = NUMBERS[random.randrange(len(NUMBERS))]
+
+    # Search for the chosen value
+    START_TIME = time.perf_counter()
+    for i in range(len(NUMBERS)):
+        if NUMBERS[i] == NUMBER:
+            print("found!")
+            break #breaks through this while loop
+    END_TIME = time.perf_counter()
+    TRIALS.append(END_TIME-START_TIME)
+
+### -- Outputs
+print(statistics.mean(TRIALS))
+
+print(NUMBER)
+print(END_TIME-START_TIME)
